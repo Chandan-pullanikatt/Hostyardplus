0f819b852a49447e0e5ccea92b78508c06319b77
Chandan Pullanikatt <chandanpullanikatt@gmail.com>
2026-05-17 12:49:42 +0530

    policy pages

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -8,7 +8,6 @@ import Image from "next/image"
 const navLinks = [
   { label: "About Us", href: "/about" },
   { label: "Partner", href: "/partner" },
-  { label: "Contact", href: "/contact" },
 ]
 
 export default function Navbar() {
